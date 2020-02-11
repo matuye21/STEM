984b5e84f80c48a6ccd62e2d49104f2195a3c4bb
matuye21 <60793456+matuye21@users.noreply.github.com>
2020-02-10 15:51:37 -1000

    Add files via upload

---
+++ readWhere.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "localhost";
+$username = "matuye21";
+$password = "uyezu";
+$dbname = "matuye21";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+
+//$sql = "SELECT name, age, gradeLevel FROM students WHERE name='Gabe' AND age=15";
+$sql = "SELECT * FROM students";
+$sql = "UPDATE students SET name='Gabe', age=16";
+$result = $conn->query($sql);
+//create query
+
+if ($result->num_rows > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+echo $row["name"]. " is ". $row["age"]." years old and in grade " . $row["gradeLevel"]; 
+echo "<br><br>";
+}
+}
+
+else {
+echo "0 results";
+}
+
+$conn->close();
+?>
